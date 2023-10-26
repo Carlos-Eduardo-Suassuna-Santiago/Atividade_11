b3e30a94f55a7e0b7842fa22555b8d4f33c9c9ab
Carlos-Eduardo-Suassuna-Santiago <carloseduardosuassunasantiago@gmail.com>
2023-10-26 19:37:23 -0300

    first commit

--- atv_11/settings.py
+++ atv_11/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Projeto.urls'
+ROOT_URLCONF = 'atv_11.urls'
 
 TEMPLATES = [
     {
--- atv_11/wsgi.py
+++ atv_11/wsgi.py
@@ -1,4 +1,5 @@
 """
+WSGI config for atv_11 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atv_11.settings')
 
 application = get_wsgi_application()
